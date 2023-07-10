70e6698a9b19798a018dfce95d91e61e0bf9ffa0
denis.pujdak <denis.pujdak@hedgehogworks.tech>
2023-07-10 17:22:01 +0200

    Use case insensitive when searching by aliases.

--- src/FluentSqlKata/FluentQueryWrapper.cs
+++ src/FluentSqlKata/FluentQueryWrapper.cs
@@ -1,13 +1,14 @@
 ﻿using SqlKata;
+using System;
 using System.Collections.Generic;
 
 namespace FluentSqlKata
 {
     internal class FluentQueryWrapper : Query
     {
-        internal readonly IDictionary<string, string> Selects = new Dictionary<string, string>(); // <alias, column>
-        internal readonly IDictionary<string, string> SelectsRaw = new Dictionary<string, string>(); // <alias, raw_query>
-        internal readonly IDictionary<string, string> SelectAggrs = new Dictionary<string, string>(); // <alias, aggregation>
+        internal readonly IDictionary<string, string> Selects = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase); // <alias, column>
+        internal readonly IDictionary<string, string> SelectsRaw = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase); // <alias, raw_query>
+        internal readonly IDictionary<string, string> SelectAggrs = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase); // <alias, aggregation>
 
         internal FluentQueryWrapper() : base() { }
 
